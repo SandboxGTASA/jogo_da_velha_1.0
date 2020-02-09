4261f29da190575115165f4e0a91a3b91b5e031a
SandboxGTASA <55704051+SandboxGTASA@users.noreply.github.com>
2020-02-09 06:11:42 -0300

    Update jogo_da_velha.py
    fiz uma pequena att

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,5 +1,5 @@
 from random import choice
-import sys, time, json
+import sys, time, json, os
 
 print('''CARA: VOCÊ É O JOGADOR "X"\nCOROA: VOCÊ É O JOGADOR "O"\nBOA SORTE!\n''')
 print("JOGANDO A MOEDA!")
@@ -13,12 +13,9 @@ print(f'\n\nO resultado foi "{e[0]}", você é o jogador "{e[1]}"!\n')
 
 T = [' '] * 9 # T de tabuleiro
 
-print(
-    '''+---------------------------------------------+\n| A tabela do jogo é representada por campos, |
-| escolha o campo que pretende preencher.     |\n+---------------------------------------------+''')
 
 def tabela():
-    m = """\n +---------------------------------------------+           
+	m = """\n +---------------------------------------------+           
 		|             Campos      Posições            |
 		|           0 | 1 | 2    {} | {} | {}            |
 		|          ---+---+---  ---+---+---           |      
@@ -26,25 +23,31 @@ def tabela():
 		|          ---+---+---  ---+---+---           |  
 		|           6 | 7 | 8    {} | {} | {}            |
 		+---------------------------------------------+"""\
-     .format(T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8])
-    ar = [line.strip() for line in m.split('\n')]
-    for line in ar:
-        print(line.strip())
+	 .format(T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8])
+	ar = [line.strip() for line in m.split('\n')]
+	for line in ar:
+	    print(line.strip())
 
-tabela()
+# tabela()
 
 def jogada(jogador):
-    pos = input(f'Jogador "{jogador}", onde pretende fazer a jogada? ')
-    while 1:
-        if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] == ' ':
-            pos = int(pos)
-            break
-        else:
-            if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] != ' ':
-                pos = input(f'Jogador "{jogador}" esse campo já foi preenchido, por favor escolha outro: ')
-            else:
-                pos = input( f'Jogador "{jogador}", digite um caractere numérico entre 0 e 8: ')
-    T[pos] = jogador
+	print(
+		'''+---------------------------------------------+\n| A tabela do jogo é representada por campos, |
+	| escolha o campo que pretende preencher.     |\n+---------------------------------------------+''')
+	tabela()
+	pos = input(f'Jogador "{jogador}", onde pretende fazer a jogada? ')
+	while 1:
+	    if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] == ' ':
+	        pos = int(pos)
+	        break
+	    else:
+	        if pos.isnumeric() and int(pos) <= 8 and T[int(pos)] != ' ':
+	            pos = input(f'Jogador "{jogador}" esse campo já foi preenchido, por favor escolha outro: ')
+	        else:
+	            pos = input( f'Jogador "{jogador}", digite um caractere numérico entre 0 e 8: ')
+	T[pos] = jogador
+	os.system('cls')
+
 
 def situacao(t, jogador): # t de vetor
     if ((t[0] == t[1] == t[2] == jogador) or\
@@ -66,13 +69,52 @@ while c <= 10:
 
     jogada(XO)
 
-    tabela()
 
     if situacao(T, XO) == 'temos um vencedor':
+        tabela()
         break
 
     if c == 9:
+        tabela()
         print("EMPATE!")
         break
-
     c = c + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input("Fim...")
